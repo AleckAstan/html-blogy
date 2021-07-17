d0bf0b8a993609a3764a156b1b4c8c5f6c4653ff
Aleck <astanaleck@gmail.com>
2021-07-17 20:36:27 +0000

    firstCommit

---
+++ script/app.js
@@ -0,0 +1,21 @@
+/***Animation On Scroll init****/
+AOS.init();
+/********READ MORE function************ */
+
+
+
+function readMore(article) {
+    let dots = document.querySelector(`.card .article[data-article="${article}"] .dots`);
+    let moreText = document.querySelector(`.card .article[data-article="${article}"] .moreText`);
+    let btnText = document.querySelector(`.card .article[data-article="${article}"] .btn`);
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.textContent = "Read more ";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.textContent = "Read less";
+        moreText.style.display = "inline";
+    }
+}
